dfaa01230fe3fd83acb70d71e0566fc8def9415d
nicoss96 <31851844+nicoss96@users.noreply.github.com>
2020-12-18 17:09:27 -0500

    Agregar archivos de proyecto.

---
+++ MatematicaFramework.Test/Calculadora.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatematicaFramework.Test
+{
+    class Calculadora
+    {
+        public int AumentarEnteros(int numeroEntero1, int numeroEntero2)
+        {
+            var suma = numeroEntero1;
+            for (var i = 0; i < numeroEntero2; i++)
+            {
+                suma++;
+            }
+            return suma;
+        }
+
+        internal double RaizCuadrada(double numero1)
+        {
+            if (numero1 < 0)
+                throw new ArgumentException("No se puede recibir numeros negativos");
+            return Math.Sqrt(numero1);
+            
+        }
+
+        public string longNombres(string nombre, string apellidoPaterno, string apellidoMaterno)
+        {
+            int total;
+            string result;
+            total = nombre.Length + apellidoPaterno.Length + apellidoMaterno.Length;
+            if (total > 50)
+            {
+                result = "Nombre demasiado largo";
+            }
+            else
+            {
+                result = "Admitido";
+            }
+            return result;
+        }
+    }
+}
---
+++ MatematicaFramework.Test/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MatematicaFramework.Test
+{
+    [TestClass]
+    public class CuandoPrueboCalculadora
+    {
+
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        //[TestMethod]
+        //[DataRow(1, 2, 3)]
+        //[DataRow(2, -3,-1)]
+        //[DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+        //   "|DataDirectory|\\data.csv", "data#csv", DataAccessMethod.Sequential),
+        // DeploymentItem("data.csv")]
+        //}
+        //[TestMethod]
+        //[DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+        //"|DataDirectory|\\data.csv", "data#csv", DataAccessMethod.Sequential),
+        //DeploymentItem("data.csv")]
+        //public void AñadirEntero()
+        //{
+
+          //  var numeroEntero1 = Int32.Parse(TestContext.DataRow["numeroEntero1"].ToString());
+           // var numeroEntero2 = Int32.Parse(TestContext.DataRow["numeroEntero2"].ToString());
+            //var esperado = Int32.Parse(TestContext.DataRow["Esperado"].ToString());
+
+            //var calculadora = new Calculadora();
+            //int resultado = calculadora.AumentarEnteros(numeroEntero1, numeroEntero2);
+            //Assert.AreEqual(esperado, resultado);
+        //}
+        [TestMethod]
+        public void SacoRaizCuadradaDeUnNumeroPositivoObtengoPositivo()
+        {
+            var numero1 = 25;
+            var esperado = 5;
+            var calculadora = new Calculadora();
+            var resultado = calculadora.RaizCuadrada(numero1);
+            Assert.AreEqual(esperado, resultado);
+        }
+        [TestMethod]
+        public void SacoRaizCuadradaDeUnNumeroNegativoSeDisparaArgumentException()
+        {
+            var numero1 = -25;
+
+            var calculadora = new Calculadora();
+
+            Assert.ThrowsException<ArgumentException>(() => calculadora.RaizCuadrada(numero1));
+        }
+
+        [TestMethod]
+        public void LongitudCorrecta()
+        {
+            //Arrange
+            string nombre = "Nicolás";
+            string apellidoPaterno = "Suárez";
+            string apellidoMaterno = "Salgado";
+            string result;
+            string resultadoEsperado = "Admitido";
+            //Act
+            var calculadora = new Calculadora();
+            result = calculadora.longNombres(nombre, apellidoPaterno, apellidoMaterno);
+            //Assert
+            Assert.AreEqual(resultadoEsperado, result);
+        }
+    }
+}
